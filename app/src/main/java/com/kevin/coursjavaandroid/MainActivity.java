57f225d71d516092f2160187654ffb332b5cd51e
Kevin Bodinaud <kevinbodinaud@hotmail.fr>
2018-10-17 10:45:30 +0200

    Boites de dialogues

--- app/src/main/java/com/kevin/coursjavaandroid/MainActivity.java
+++ app/src/main/java/com/kevin/coursjavaandroid/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kevin.coursjavaandroid;
 
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,13 +12,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
 
     private Button btAnnuler;
     private RadioButton rbJaime;
@@ -29,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private final static int ALERT_DIALOG = 1;
     private final static int TIME_PICKER = 2;
     private final static int DATE_PIKER = 3;
+    private static final SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,6 +91,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         menu.add(0,TIME_PICKER,0,"timePicker");
         menu.add(0,DATE_PIKER,0,"datePicker");
 
+
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -117,12 +126,35 @@ switch (item.getItemId()){
         TimePickerDialog timePickerDialog = new TimePickerDialog(this, this, 14, 33, true);
         timePickerDialog.show();
         break;
+
+    case DATE_PIKER:
+        //Gestion de la date
+        Calendar calendar = Calendar.getInstance();
+//Création de la fenêtre
+//Pour le callback -> Alt+entree -> implémente méthode -> Génère la méthode onTimeSet
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, this,
+                calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH));
+//Afficher la fenêtre
+        datePickerDialog.show();
 }
 
         return super.onOptionsItemSelected(item);
     }
 
 
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Toast.makeText(this, hourOfDay +":" + minute, Toast.LENGTH_LONG).show();
+    }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
+        Calendar calendar= Calendar.getInstance();
+        calendar.set(year,month,dayOfMonth);
+        String dateString  = format.format(calendar.getTime());
+
+        Toast.makeText(this, dateString, Toast.LENGTH_SHORT).show();
+    }
 }
